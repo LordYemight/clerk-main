927067592050fb06b32de1f504731c836b8cf707
lordyemight <lordyemight@gmail.com>
2024-07-23 15:50:01 +0100

    clerk with backend

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-    publicRoutes: ["/", "/login", "/register", "/forgetpassword"]
+    publicRoutes: ["/", "/login", "/register", "/forgetpassword", "/ api/webhooks/clerk"]
 });
 
 export const config = {
